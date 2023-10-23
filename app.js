ea3b482f28cf1c622e21f36faa0a985ed4fc8b4b
kostiantyn <kostiantyn@test.com>
2023-10-23 14:51:38 -0230

    initialized token feature

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("Hello World");
+const { argv } = require("process");
+
+const { tokenFeature } = require("./utils-token");
+
+const commandsArr = argv.slice(2);
+const feature = commandsArr[0];
+const featureOptions = commandsArr.slice(1);
+
+switch (feature) {
+  case "token":
+  case "t":
+    tokenFeature();
+}
+
+module.exports = {
+  featureOptions,
+};
