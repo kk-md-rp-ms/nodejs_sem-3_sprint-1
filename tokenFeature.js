353d8747bb9acc961beaefb88b784e33f4564ccc
kostiantyn <kostiantyn@test.com>
2023-10-25 14:33:39 -0230

    changed paths declaration

--- tokenFeature.js
+++ tokenFeature.js
@@ -1,6 +1,11 @@
 const { createToken, createNewUserObj, saveToken } = require("./utils-token");
 const { fetchFile } = require("./utils-fs");
-const { tokenExpiresDays, tokenFolder, tokenFile } = require("./defaults");
+const {
+  userCfgFilePath,
+  tokenCfgFilePath,
+  tokenExpiresDays,
+  allTokensFilePath,
+} = require("./defaults");
 
 const tokenFeature = (optionsArr) => {
   switch (optionsArr[0]) {
@@ -20,13 +25,9 @@ const tokenFeature = (optionsArr) => {
       (async () => {
         try {
           // issue-#23: add functionality if "user-config.json" and "token-config.json" weren't initialized
-          const userTemplate = JSON.parse(
-            await fetchFile("json", "user-config.json")
-          );
+          const userTemplate = JSON.parse(await fetchFile(userCfgFilePath));
 
-          const tokenTemplate = JSON.parse(
-            await fetchFile("json", "token-config.json")
-          );
+          const tokenTemplate = JSON.parse(await fetchFile(tokenCfgFilePath));
 
           const userTemplateKeysArr = Object.keys(userTemplate);
 
@@ -49,7 +50,7 @@ const tokenFeature = (optionsArr) => {
             tokenExpiresDays
           );
 
-          saveToken(newTokenObj, tokenFolder, tokenFile);
+          saveToken(newTokenObj, allTokensFilePath);
         } catch ({ name, message }) {
           console.log(`${name}: ${message}`);
         }
