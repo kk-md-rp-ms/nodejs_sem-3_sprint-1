f7e44f191e55620472c4cabec884b796dc9448c7
kostiantyn <kostiantyn@test.com>
2023-10-25 11:41:55 -0230

    +saveToken function
    Additionally changed output format for createdDate and expiresDate

--- utils-token.js
+++ utils-token.js
@@ -1,6 +1,8 @@
 const crc32 = require("crc/crc32");
 const { format } = require("date-fns");
 
+const { fetchFile, createFolder, createFile } = require("./utils-fs");
+
 const createNewUserObj = (keysArr, ...args) => {
   const newUserObj = {};
 
@@ -26,15 +28,35 @@ const createToken = (userObj, tokenObj, ttlDays) => {
   return newTokenObj;
 };
 
+const saveToken = async (tokenObj, folder, fileName) => {
+  let dataArr = (await fetchFile(folder, fileName)) || [];
+
+  if (!Array.isArray(dataArr)) {
+    try {
+      dataArr = JSON.parse(dataArr);
+    } catch (err) {
+      dataArr = [];
+      console.log(err);
+    }
+  }
+
+  dataArr.push(tokenObj);
+  await createFolder(folder);
+  await createFile(JSON.stringify(dataArr, null, 2), folder, fileName);
+};
+
 const getTokenLifeSpan = (ttlDays) => {
   const createdDate = Date.now();
   const expiresDate = new Date();
 
   expiresDate.setDate(expiresDate.getDate() + ttlDays);
-  return [createdDate, expiresDate].map((item) => format(item, "dd/MM/yyyy"));
+  return [createdDate, expiresDate].map((item) =>
+    format(item, "yyyy-MM-dd HH:mm:ss")
+  );
 };
 
 module.exports = {
   createToken,
   createNewUserObj,
+  saveToken,
 };
