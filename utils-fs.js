aab776165f5766c2040ddeb31461d340cb466a8a
kostiantyn <kostiantyn@test.com>
2023-10-25 11:40:14 -0230

    +createFolder, +createFile functions
    Also renamed '...args' to '...pathArgs'

--- utils-fs.js
+++ utils-fs.js
@@ -1,8 +1,8 @@
-const { readFile } = require("node:fs/promises");
+const { readFile, writeFile, mkdir } = require("node:fs/promises");
 const { join } = require("node:path");
 
-const fetchFile = async (...args) => {
-  const path = join(__dirname, ...args);
+const fetchFile = async (...pathArgs) => {
+  const path = join(__dirname, ...pathArgs);
 
   try {
     const data = await readFile(path);
@@ -12,6 +12,30 @@ const fetchFile = async (...args) => {
   }
 };
 
+const createFolder = async (...pathArgs) => {
+  const path = join(__dirname, ...pathArgs);
+
+  try {
+    await mkdir(path);
+  } catch ({ name, message, code }) {
+    code == "EEXIST"
+      ? console.log("folder already exist")
+      : console.log(`${name}: ${message}`);
+  }
+};
+
+const createFile = async (content, ...pathArgs) => {
+  const path = join(__dirname, ...pathArgs);
+
+  try {
+    await writeFile(path, content);
+  } catch ({ name, message }) {
+    console.log(`${name}: ${message}`);
+  }
+};
+
 module.exports = {
   fetchFile,
+  createFolder,
+  createFile,
 };
