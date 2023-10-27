d4e056b19f2b79861ea91e70f9e7962700c63fd3
kostiantyn <kostiantyn@test.com>
2023-10-27 19:59:06 -0230

    +logEE.logFile() for logging

--- tokenFeature.js
+++ tokenFeature.js
@@ -1,4 +1,5 @@
 // Import required functions/variables from custom modules
+const logEE = require("./log-emitter");
 const {
   processTokenHelp,
   processTokenCount,
@@ -12,22 +13,27 @@ const tokenFeature = async (optionsArr) => {
   switch (optionsArr[0]) {
     case "--help":
       // displays help for the token command
+      logEE.logFile("tokenFeature", "info", `Access to the "--help" option`);
       await processTokenHelp(optionsArr.slice(1));
       break;
     case "--count":
       // displays a count of the tokens created
+      logEE.logFile("tokenFeature", "info", `Access to the "--count" option`);
       await processTokenCount(optionsArr.slice(1));
       break;
     case "--new":
       // generates a token for a given data, saves tokens to the json file
+      logEE.logFile("tokenFeature", "info", `Access to the "--new" option`);
       await processTokenCreate(optionsArr.slice(1));
       break;
     case "--upd":
       // updates the json entry with...options...
+      logEE.logFile("tokenFeature", "info", `Access to the "--upd" option`);
       await processTokenUpdate(optionsArr.slice(1));
       break;
     case "--search":
       // fetches a token for a given...options...
+      logEE.logFile("tokenFeature", "info", `Access to the "--search" option`);
       await processTokenSearch(optionsArr.slice(1));
       break;
   }
