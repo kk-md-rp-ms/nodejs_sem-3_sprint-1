1f84d41ebee0dbb92b6b03769f610925abd09cdc
kostiantyn <kostiantyn@test.com>
2023-10-28 18:14:21 -0230

    added logging to the console

--- utils-log.js
+++ utils-log.js
@@ -7,6 +7,28 @@ const { format } = require("date-fns");
 
 // Import required functions/variables from custom modules
 const { logFolder, logSubFolder, logFile } = require("./defaults");
+const {
+  flagDevMode,
+  logEvent,
+  logLevelError,
+  logLevelWarning,
+  logLevelSuccess,
+  logLevel,
+  logMessage,
+} = require("./defaults-dev-mode");
+
+// Create a developer log entry
+const createDevLog = (event, level, message) => {
+  return `${logEvent(event)} - ${
+    level == "error"
+      ? logLevelError(level)
+      : level == "warning"
+      ? logLevelWarning(level)
+      : level == "success"
+      ? logLevelSuccess(level)
+      : logLevel(level)
+  }\t[${["error", "warning"].includes(level) ? message : logMessage(message)}]`;
+};
 
 // Create a log entry
 const createLog = (event, level, message) => {
@@ -24,20 +46,46 @@ const saveLog = async (log) => {
 
     // Create the sub directory if it doesn't exist
     await mkdir(logSubFolder(), { recursive: true });
-  } catch ({ name, message, code }) {
-    // log directory creation errors
-    code != "EEXIST" && console.log(`${name}: ${message}`);
+
+    // Display log entry in the console if developer mode is active
+    flagDevMode &&
+      console.log(
+        createDevLog(
+          "saveLog",
+          "success",
+          `Folder: ${logSubFolder()} was created successfully`
+        )
+      );
+  } catch ({ message, code }) {
+    // Display log entry in the console if developer mode is active
+    if (flagDevMode) {
+      code == "EEXIST"
+        ? console.log(createDevLog("saveLog", "warning", `${message}`))
+        : console.log(createDevLog("saveLog", "error", `${message}`));
+    }
   }
 
   // Append the log data to the file
   try {
     await appendFile(logFile(), log);
-  } catch ({ name, message }) {
-    console.log(`${name}: ${message}`);
+
+    // Display log entry in the console if developer mode is active
+    flagDevMode &&
+      console.log(
+        createDevLog(
+          "saveLog",
+          "success",
+          `log was successfully written to the ${logFile()}`
+        )
+      );
+  } catch ({ message }) {
+    // Display log entry in the console if developer mode is active
+    flagDevMode && console.log(createDevLog("saveLog", "error", `${message}`));
   }
 };
 
 module.exports = {
+  createDevLog,
   createLog,
   saveLog,
 };
