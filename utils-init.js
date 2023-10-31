b1a40fd407d6bc0fb9ac78299fe9d6a9a9f98c47
kostiantyn <kostiantyn@test.com>
2023-10-31 10:57:59 -0230

    +processInitAll, +processInitCat, +processInitMk
    Only initialized these functions. Will finish them later

--- utils-init.js
+++ utils-init.js
@@ -33,6 +33,48 @@ const processInitHelp = async (optionsArr) => {
     : logEE.logToFile("processInitHelp", "error", `"help" file not found`);
 };
 
+// Functions to process the init option "--all"
+const processInitAll = async (optionsArr) => {
+  // Check if there are extra arguments after "--all"
+  if (optionsArr.length) {
+    // Provide feedback
+    console.log("Invalid syntax");
+
+    // Write log to the file
+    logEE.logToFile("processInitAll", "warning", "Invalid syntax");
+    return;
+  }
+
+  // processInitCat,
+  // processInitMk
+};
+
+// Functions to process the init option "--cat"
+const processInitCat = async (optionsArr) => {
+  // Check if there are extra arguments after "--cat"
+  if (optionsArr.length) {
+    // Provide feedback
+    console.log("Invalid syntax");
+
+    // Write log to the file
+    logEE.logToFile("processInitCat", "warning", "Invalid syntax");
+    return;
+  }
+};
+
+// Functions to process the init option "--mk"
+const processInitMk = async (optionsArr) => {
+  // Check if there are extra arguments after "--mk"
+  if (optionsArr.length) {
+    // Provide feedback
+    console.log("Invalid syntax");
+
+    // Write log to the file
+    logEE.logToFile("processInitMk", "warning", "Invalid syntax");
+    return;
+  }
+};
+
 module.exports = {
   processInitHelp,
 };
