383388b7e95a807792ace55309fe363b6e96bcf1
kostiantyn <kostiantyn@test.com>
2023-11-01 10:57:29 -0230

    ++createFolderWithFile, simplified code

--- configFeature.js
+++ configFeature.js
@@ -1,23 +1,14 @@
-// Import required functions/variables from built-in modules
-const { dirname } = require("node:path");
-
 // Import required functions/variables from custom modules
 const logEE = require("./log-emitter");
 const { cfgHelpFilePath, fileContentMap } = require("./defaults");
-const { createFolder, createFile } = require("./utils-fs");
+const { createFolderWithFile } = require("./utils-fs");
 
 const { processCfgHelp } = require("./utils-config");
 
 // Define a function to handle the config feature based on provided options
 const configFeature = async (optionsArr) => {
   // Create the config help folder and file as soon as the configFeature is accessed
-  try {
-    createFolder(dirname(cfgHelpFilePath));
-    createFile(fileContentMap.get(cfgHelpFilePath), cfgHelpFilePath);
-  } catch (err) {
-    // Handle and log any errors that occur during folder/file creation
-    logEE.logToFile("configFeature", "error", err.message);
-  }
+  createFolderWithFile(cfgHelpFilePath, fileContentMap.get(cfgHelpFilePath));
 
   switch (optionsArr[0]) {
     case "--help":
