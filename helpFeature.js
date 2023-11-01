e8af868e4e76b2c6df54230f23597163290fa48a
kostiantyn <kostiantyn@test.com>
2023-11-01 12:09:10 -0230

    +helpFeature.js

---
+++ helpFeature.js
@@ -0,0 +1,33 @@
+// Import required functions/variables from custom modules
+const logEE = require("./log-emitter");
+const { notFoundMessage, appHelpFilePath } = require("./defaults");
+const { fetchTxtFile } = require("./utils-fs");
+
+// Function to process "--help"
+const helpFeature = async (optionsArr) => {
+  // Check if there are extra arguments after "--help"
+  if (optionsArr.length) {
+    // Provide feedback
+    console.log("Invalid syntax");
+
+    // Write log to the file
+    logEE.logToFile("helpFeature", "warning", "Invalid syntax");
+    return;
+  }
+
+  // Read the help text from the specified file
+  const data = (await fetchTxtFile(appHelpFilePath)) || notFoundMessage;
+
+  // Display result
+  console.log(data);
+
+  // Write log to the file
+  data !== notFoundMessage
+    ? logEE.logToFile("helpFeature", "success", `"help" file was displayed`)
+    : logEE.logToFile("helpFeature", "error", `"help" file not found`);
+};
+
+// Export all the defined variables for use in other modules
+module.exports = {
+  helpFeature,
+};
